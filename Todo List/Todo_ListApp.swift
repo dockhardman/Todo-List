86feebcdd60018752dfa696b8ac5852b01b94a16
Allen Chou <f1470891079@gmail.com>
2023-06-22 22:26:45 +0800

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Allen Chou on 2023/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
